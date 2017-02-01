fcbbb4c9734486b2daa6e0e7705d6d4b6c0df67d
K8964 <K8964@student.jamk.fi>
2017-02-01 12:53:25 +0200

    1.t

---
+++ Harj1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Harj4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            teksti();
+        }
+        static void teksti()
+        {
+            string riviloppu;
+            string minunkansio = @"d:\k8964\tekstikansio.txt";
+            bool repeat = true;
+
+            while (repeat == true)
+            {
+            Console.Write("Anna tekstiä ('0' niin loppuu ) : ");
+            riviloppu = Console.ReadLine();
+            if (riviloppu == "0") { repeat = false; }
+            else
+            {
+                    try
+                    {
+                    using (StreamWriter writer = new StreamWriter(minunkansio, true))
+                    {
+                    writer.WriteLine(riviloppu);
+                    }
+                    }
+                    catch (Exception ex)
+                    {
+                    Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            Console.WriteLine("Sisältö " + minunkansio + ":");
+            try
+            {
+            List<string> rivi = new List<string>(File.ReadAllLines(minunkansio));
+            foreach (string RIVI in rivi) {Console.WriteLine(RIVI);}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
