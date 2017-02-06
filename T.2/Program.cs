0df9f28a9a6f8df057b0efd9ae1f1bc69896faf7
K8964 <K8964@student.jamk.fi>
2017-02-06 09:44:11 +0200

    teht2

---
+++ T.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T2;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaaOstos();
+        }
+        static void TestaaOstos()
+        {
+        List<Ostos> ostokset = new List<Ostos>();
+        ostokset.Add(new Ostos("Limsa", 5));
+        ostokset.Add(new Ostos("Makkara", 4)); 
+        ostokset.Add(new Ostos("Limppu", 3)); 
+        ostokset.Add(new Ostos("ES ENERGIA JUOMA", 5));  
+        
+    Console.WriteLine("Kaikki kärryssä olevat ostokset ");
+        foreach (Ostos ostos in  ostokset)
+         {
+                Console.WriteLine(ostos.ToString());
+            }
+    }
+}
+}
---
+++ T.2/ostos.cs
@@ -0,0 +1,22 @@
+﻿namespace T2
+{
+    class Ostos
+    {
+        public string Nimi { get; set; }
+
+        public float Hinta { get; set; }
+
+        public Ostos(string nimi, float hinta)
+        {
+
+            Nimi = nimi;
+
+            Hinta = hinta;
+        }
+            public override string ToString()
+        {
+            return "Ostos " + Nimi + " " + Hinta + " euroa";
+        }
+    }
+    
+}
